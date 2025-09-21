1fc19964303138d09907917d6412ec6e888da31a
ciodar <dariocioni03@gmail.com>
2025-09-21 11:37:12 +0200

    Add flux kontext

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from qwenaug.model import download_model_weights
+from qwenaug.model import download_qwen_weights,download_kontext_weights
 
 def main():
     print("Hello from qwen-synthetic-augmentation!")
-    download_model_weights()
+    download_kontext_weights()
     
 
 if __name__ == "__main__":
--- qwenaug/model.py
+++ qwenaug/model.py
@@ -4,7 +4,7 @@ import torch
 
 from huggingface_hub import hf_hub_download
 
-def download_model_weights(directory='weights'):
+def download_qwen_weights(directory='weights'):
     original_repo = 'Qwen/Qwen-Image-Edit'
     quantized_repo = 'QuantStack/Qwen-Image-Edit-GGUF'
     # Download vae
@@ -14,6 +14,72 @@ def download_model_weights(directory='weights'):
     hf_hub_download(repo_id=quantized_repo, filename='Qwen_Image_Edit-Q4_0.gguf', local_dir='weights/transformer')
     # os.makedirs('weights/text_encoder',exist_ok=True)
     # hf_hub_download(repo_id='Qwen/Qwen2.5-VL-7B-Instruct',  local_dir='weights/transformer')
+
+def download_kontext_weights(directory='weights'):
+    original_repo = 'black-forest-labs/FLUX.1-Kontext-dev'
+    quantized_repo = 'QuantStack/FLUX.1-Kontext-dev-GGUF'
+    os.makedirs('weights/vae',exist_ok=True)
+    hf_hub_download(repo_id=original_repo, filename='vae/diffusion_pytorch_model.safetensors', local_dir='weights')
+    os.makedirs('weights/transformer',exist_ok=True)
+    hf_hub_download(repo_id=quantized_repo, filename='flux1-kontext-dev-Q2_K.gguf', local_dir='weights/transformer')
+    os.makedirs('weights/text_encoder/t5', exist_ok=True)
+    hf_hub_download(repo_id='city96/t5-v1_1-xxl-encoder-gguf', filename='t5-v1_1-xxl-encoder-Q3_K_M.gguf', local_dir='weights/text_encoder/t5')
+
+def load_flux_kontext():
+    from diffusers import FluxKontextPipeline, GGUFQuantizationConfig, FluxTransformer2DModel
+    from diffusers.hooks import apply_layerwise_casting, apply_group_offloading
+    from transformers import T5EncoderModel
+    quantization_config=GGUFQuantizationConfig(compute_dtype=torch.bfloat16)
+    transformer = FluxTransformer2DModel.from_single_file(
+        pretrained_model_link_or_path_or_dict='weights/transformer/flux1-kontext-dev-Q2_K.gguf',
+        quantization_config=quantization_config,
+        config="weights/transformer/config.json",
+        dtype=torch.bfloat16
+    )
+    # transformer.enable_layerwise_casting(storage_dtype=torch.float8_e4m3fn, compute_dtype=torch.bfloat16)
+    text_encoder_2 = T5EncoderModel.from_pretrained(
+        'city96/t5-v1_1-xxl-encoder-gguf',
+        gguf_file='t5-v1_1-xxl-encoder-Q3_K_M.gguf',
+        load_in:
+        # quantization_config=quantization_config,
+        # config="weights/text_encoder/t5/config.json",
+        dtype=torch.bfloat16
+    )
+    # apply_layerwise_casting(
+    #     text_encoder_2,
+    #     storage_dtype=torch.float8_e4m3fn,
+    #     compute_dtype=torch.bfloat16,
+    #     non_blocking=True,
+    # )
+
+    pipe = FluxKontextPipeline.from_pretrained(
+        "black-forest-labs/FLUX.1-Kontext-dev", 
+        transformer=transformer,
+        text_encoder_2=text_encoder_2,
+        torch_dtype=torch.bfloat16, 
+        map_device="balanced"
+    )
+    # apply_layerwise_casting(
+    #     pipe.text_encoder,
+    #     storage_dtype=torch.float8_e4m3fn,
+    #     compute_dtype=torch.bfloat16,
+    #     non_blocking=True,
+    # )
+    apply_layerwise_casting(
+        pipe.transformer,
+        storage_dtype=torch.float8_e4m3fn,
+        compute_dtype=torch.bfloat16,
+        non_blocking=True,
+    )
+    onload_device = torch.device("cuda")
+    offload_device = torch.device("cpu")
+    apply_group_offloading(pipe.text_encoder, onload_device=onload_device, offload_type="block_level", num_blocks_per_group=2, use_stream=True)
+    apply_group_offloading(pipe.text_encoder_2, onload_device=onload_device, offload_type="block_level", num_blocks_per_group=2, use_stream=True)
+    pipe.transformer.enable_group_offload(onload_device=onload_device, offload_device=offload_device, offload_type="leaf_level", use_stream=True, record_stream=True)
+    pipe.vae.enable_group_offload(onload_device=onload_device, offload_type="leaf_level", use_stream=True, record_stream=True)
+    # pipe.to("cuda")
+    return pipe
+
 
 def load_qwen_image_edit():
     from diffusers import QwenImageEditPipeline, AutoencoderKLQwenImage, QwenImageTransformer2DModel, GGUFQuantizationConfig
@@ -84,7 +150,7 @@ def load_qwen_image_edit():
     # Use the apply_group_offloading method for other model components
     apply_group_offloading(pipeline.text_encoder, onload_device=onload_device, offload_type="block_level", num_blocks_per_group=2, use_stream=True, record_stream=True)
 
-    pipeline.to("cuda")
+    # pipeline.to("cuda")
     # pipeline.set_progress_bar_config(disable=None)
     
     return pipeline
@@ -93,7 +159,7 @@ def load_qwen_image_edit():
 
 if __name__ == "__main__":
     # Example usage:
-    pipeline = load_qwen_image_edit()
+    pipeline = load_flux_kontext()
     image = Image.open("examples/input.jpg").convert("RGB")
     prompt = "Change the rabbit's color to purple, with a flash light background."
     inputs = {
